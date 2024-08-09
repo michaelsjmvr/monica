125458cad96799f0faa9885b338ac340f60c825d
Michael Douglas <michaelsjmvr@hotmail.com>
2024-08-09 18:04:09 -0300

    Menu Desktop e mobile, logo

---
+++ src/javascript/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.menu-toggle-btn').addEventListener('click', function() {
+    const menu = document.querySelector('.menu-vertical');
+    menu.classList.toggle('open');
+});
